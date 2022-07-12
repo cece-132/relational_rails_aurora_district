399a938e9519b184d8b6a272100d9d3c39479437
Candace Eckels <candace.eckels@gmail.com>
2022-07-12 09:40:44 -0600

    Created seed data and daycares show on html screen

---
+++ app/controllers/daycares_controller.rb
@@ -0,0 +1,5 @@
+class DaycaresController < ApplicationController
+    def index
+        @daycares = Daycare.all
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@daycare_1 = Daycare.create!(name: "Aurora's Promise", total_teachers: 15, total_students: 225, enrollment_full: false)
+@daycare_2 = Daycare.create!(name: "Little Tykes - Aurora", total_teachers: 10, total_students: 150, enrollment_full: false)
+@daycare_3 = Daycare.create!(name: "Einstein's Daycare", total_teachers: 8, total_students: 120, enrollment_full: false)
